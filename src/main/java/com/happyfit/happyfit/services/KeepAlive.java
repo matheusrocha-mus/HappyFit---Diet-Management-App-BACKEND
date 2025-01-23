b30004d3d32a0d1fb070a227a1a2d4f23332536e
Matheus Rocha <matheuscaetanorocha@gmail.com>
2025-01-23 02:23:05 -0300

    feat: keep-alive cron job

---
+++ src/main/java/com/happyfit/happyfit/services/KeepAlive.java
@@ -0,0 +1,32 @@
+package com.happyfit.happyfit.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.happyfit.happyfit.models.User;
+
+import com.happyfit.happyfit.seeders.DatabaseSeeder;
+
+import jakarta.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Service
+public class KeepAlive {
+
+    @Autowired
+    private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseSeeder.class);
+
+    @Transactional
+    @Scheduled(cron = "0 0,15,30,45 7-23 * * *")
+    public void keepAlive() {
+        logger.info("KEEP ALIVE CRON");
+        User user = userService.findByEmail("admin@example.com");
+        if (user != null) logger.info("KEEP ALIVE SUCCESFULLY FETCHED " + user.getName() + " USER");
+        else logger.error("KEEP ALIVE FAILED TO FETCH USER");
+    }
+}
